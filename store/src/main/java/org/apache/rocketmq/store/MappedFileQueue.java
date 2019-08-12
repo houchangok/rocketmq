ec39494a220c921faba6c6de9b2c942a0d305157
xn092072 <houzhangyuan@xiaoniu88.com>
2019-08-12 13:37:46 +0800

    rocketmq源码阅读：消息存储机制

--- store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
+++ store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
@@ -29,16 +29,29 @@ import org.apache.rocketmq.common.constant.LoggerName;
 import org.apache.rocketmq.logging.InternalLogger;
 import org.apache.rocketmq.logging.InternalLoggerFactory;
 
+/**
+ * 消息存储：对应存储目录下面的文件夹
+ */
 public class MappedFileQueue {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.STORE_LOGGER_NAME);
     private static final InternalLogger LOG_ERROR = InternalLoggerFactory.getLogger(LoggerName.STORE_ERROR_LOGGER_NAME);
 
     private static final int DELETE_FILES_BATCH_MAX = 10;
 
+    /**
+     * 存储目录地址
+     *
+     */
     private final String storePath;
 
+    /**
+     *单个文件的存储大小
+     */
     private final int mappedFileSize;
 
+    /**
+     * 文件集合，采用的数据结构是并发容器
+     */
     private final CopyOnWriteArrayList<MappedFile> mappedFiles = new CopyOnWriteArrayList<MappedFile>();
 
     private final AllocateMappedFileService allocateMappedFileService;
